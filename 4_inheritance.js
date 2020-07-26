87d6604137166766768701d9b587e2b9a15aa187
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 13:45:53 +0100

    inheritance

--- 4_inheritance.js
+++ 4_inheritance.js
@@ -6,3 +6,7 @@ function Book(title, author, year) {
 Book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 }
+
+function Magazine(title,author,year,month){
+    Book.call(this,title,author,year)
+}
