b9eb40105ee88c588a2ed1a8a23104a1a5d1e278
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 01:52:18 +0100

    constructor

--- 2_constructor.js
+++ 2_constructor.js
@@ -2,7 +2,13 @@ function Book(title, author, year){
     this.title = title;
     this.author = author;
     this.year = year;
+
+    this.getSummary = function(){
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
 }
 
 const book1 = new Book('Book One', 'John Doe', '2016');
 const book2 = new Book('Book two', 'Jane Doe', '2017');
+
+console.log(book2);
