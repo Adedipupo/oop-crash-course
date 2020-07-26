7dd363f273e4b4b7101bd264fd1e1489665f54e0
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 13:30:22 +0100

    prototypes

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -7,8 +7,12 @@ function Book(title, author, year) {
 Book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 }
+Book.prototype.getAge = function () {
+    const years = new Date().getFullYear - this.year;
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+}
 
 const book1 = new Book('Book One', 'John Doe', '2016');
 const book2 = new Book('Book two', 'Jane Doe', '2017');
 
-console.log(book1.getSummary());
+console.log(book2);
