334c6f64672beb9ed93712133c4535576f2bf3f6
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 14:19:06 +0100

    sub_classes

---
+++ 7_subclasses.js
@@ -0,0 +1,16 @@
+class Book {
+    constructor(title, author, year) {
+        this.title = title,
+            this.author = author,
+            this.year = year
+    }
+    getSummary() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+}
+
+class Magazine extends Book {
+    constructor(title,author,year,month){
+        super
+    }
+}
