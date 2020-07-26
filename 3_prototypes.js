3c5098b837a62adf68181bfca71e49a82d7e5a34
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 01:54:08 +0100

    prototypes

---
+++ 3_prototypes.js
@@ -0,0 +1,14 @@
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummary = function () {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+}
+
+const book1 = new Book('Book One', 'John Doe', '2016');
+const book2 = new Book('Book two', 'Jane Doe', '2017');
+
+console.log(book2.getSummary());
