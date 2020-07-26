02225184ade03ec6998f0ad928c030312b035a34
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 13:32:06 +0100

    prototypes

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -8,11 +8,11 @@ Book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 }
 Book.prototype.getAge = function () {
-    const years = new Date().getFullYear - this.year;
-    return `${this.title} was written by ${this.author} in ${this.year}`;
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} old`;
 }
 
 const book1 = new Book('Book One', 'John Doe', '2016');
 const book2 = new Book('Book two', 'Jane Doe', '2017');
 
-console.log(book2);
+console.log(book2.getAge());
