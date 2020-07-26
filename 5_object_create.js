c199da79a51ff209a4f7ebf76757e0217d4264ed
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-26 14:01:32 +0100

    object_create

--- 5_object_create.js
+++ 5_object_create.js
@@ -1,6 +1,8 @@
 const bookProtos = {
     getSummary: function () {
         return `${this.title} was written by ${this.author} in ${this.year}`;
+    },
+    getAge: function(){
+        return `${this.title} is ${years} years old`;
     }
-    get
 }
